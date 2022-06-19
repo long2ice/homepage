cee60220df633916bd79274403856817696f916c
long2ice <long2ice@gmail.com>
2022-06-19 19:12:35 +0800

    update favicon

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
-import {App} from "./App"
+import {App} from "./app"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
