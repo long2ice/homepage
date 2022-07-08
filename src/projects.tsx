c00f3219a938ca3460a3173959783ec68675790a
long2ice <long2ice@gmail.com>
2022-07-08 23:48:42 +0800

    add products

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,8 +1,24 @@
-import { Grid, GridItem, Heading, Image, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Text,
+} from "@chakra-ui/react";
 
 interface Repo {
   username: string;
   repo: string;
+}
+
+interface Product {
+  title: string;
+  description: string;
+  link: string;
 }
 
 function Projects(props: Record<string, any>) {
@@ -36,11 +52,51 @@ function Projects(props: Record<string, any>) {
       repo: "asyncmy",
     },
   ];
+  const products: Array<Product> = [
+    {
+      title: "TelSearch",
+      description: "Search for telegram",
+      link: "https://telsearch.long2ice.io",
+    },
+    {
+      title: "Awesome",
+      description: "Search for awesome projects",
+      link: "https://awesome.long2ice.io",
+    },
+  ];
   return (
     <div {...props}>
-      <Text fontSize="4xl" as="em">
-        My Projects
-      </Text>
+      <div>
+        <Text fontSize="4xl" as="em">
+          My Awesome Products
+        </Text>
+        <Grid mt="2%" templateColumns="repeat(3, 1fr)">
+          {products.map((product) => (
+            <GridItem>
+              <LinkBox textDecoration="none">
+                <Box
+                  maxW="sm"
+                  borderWidth="1px"
+                  borderRadius="5px"
+                  overflow="hidden"
+                  padding={2}
+                >
+                  <Text fontSize="xl" fontWeight="medium" color="#3081ED">
+                    {product.title}
+                  </Text>
+                  <Text color="#434D58">{product.description}</Text>
+                </Box>
+                <LinkOverlay href={product.link} isExternal />
+              </LinkBox>
+            </GridItem>
+          ))}
+        </Grid>
+      </div>
+      <Box mt="2%">
+        <Text fontSize="4xl" as="em">
+          My Open Source Projects
+        </Text>
+      </Box>
       <Grid mt="2%" gap={4} templateColumns="repeat(3, 1fr)">
         {projects.map((p) => (
           <GridItem>
